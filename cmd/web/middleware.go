1a7d43dcb0d35feab5acaa26f5801d56ce449caa
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2022-12-22 19:23:27 +0100

    Fixed typo

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -8,16 +8,16 @@ import (
 
 // NoSurf adds CSRF protection to all POST request
 func NoSurf(next http.Handler) http.Handler {
-	csrfHander := nosurf.New(next)
+	csrfHandler := nosurf.New(next)
 
-	csrfHander.SetBaseCookie(http.Cookie{
+	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
 		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 
-	return csrfHander
+	return csrfHandler
 }
 
 // SessionLoad loads and saves the session on every request
