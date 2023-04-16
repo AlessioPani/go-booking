21dcaccf7c69f1bb0b473d712872c479b51e9399
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2023-04-16 16:28:54 +0200

    Added migrations to generate rooms and restrictions

---
+++ migrations/20230416142707_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+                                                                             ('Reservation','2023-04-15 00:00:00','2023-04-15 00:00:00'),
+                                                                             ('Owner Block','2023-04-16 00:00:00','2023-04-16 00:00:00');
